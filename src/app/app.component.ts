2899aa78f6b49b306173079e501cbd5c5290891f
Azathoth <vitalii.glinka@yahoo.com>
2020-05-06 23:42:25 +0300

    Sign in/out from sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,24 @@ import { ConfirmDialogService } from './common/confirm_dialog/confirm-dialog.ser
 import { PuzzleTypeModel } from './models/puzzle-types/puzzle-type.model';
 import { PuzzleLookupService } from './services/puzzle-lookup.service';
 import { Router } from '@angular/router';
+import { UserRegistrationComponent } from './components/account/registration/user-registration.component';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit, OnInit {
+export class AppComponent implements AfterViewInit, OnInit, OnDestroy {
 	title = 'puzzleshop-client';
 
 	puzzleTypes: PuzzleTypeModel[] = [];
 	rcPuzzles: PuzzleTypeModel[] = [];
 	wcaPuzzles: PuzzleTypeModel[] = [];
+
+	
+	dialogRefSubscription: Subscription;
+	dialogRefSubscription1: Subscription;
+	subscriptions: Subscription[] = [];
 
 	public get isLoggedIn(): boolean {
 		return this.accountService.isAuthenticated();
@@ -34,7 +40,10 @@ export class AppComponent implements AfterViewInit, OnInit {
 
 	constructor(private accountService: AccountService, 
 				private router: Router,
-				private lookupService: PuzzleLookupService) {
+				private dialogService: ConfirmDialogService,
+				private notificationService: NotificationService,
+				private lookupService: PuzzleLookupService,
+				private matDialog: MatDialog) {
 	}
 
 	ngOnInit(): void {
@@ -42,6 +51,44 @@ export class AppComponent implements AfterViewInit, OnInit {
 	}
 
 	onClick = (puzzleType: string) => this.router.navigate(['/collections', puzzleType]);
+
+	onSignInClick(): void {
+		const dialogConfig = new MatDialogConfig();
+		dialogConfig.autoFocus = true;
+		dialogConfig.disableClose = false;
+		dialogConfig.height = "70%";
+		dialogConfig.width = "30%";
+		const dialogRef: MatDialogRef<UserLoginComponent> = this.matDialog.open(UserLoginComponent, dialogConfig);
+		this.dialogRefSubscription = dialogRef.afterClosed().subscribe(() => {
+			this.ngOnInit();
+		});
+		this.subscriptions.push(this.dialogRefSubscription);
+	}
+
+	onSignOutClick(): void {
+		const dialogRef = this.dialogService.openConfirmDialog('Do You really wish to logout?');
+		this.dialogRefSubscription1 = dialogRef.afterClosed().subscribe(action => {
+			if (action === dialogRef.componentInstance.ACTION_CONFIRM) {
+				this.accountService.logout()
+					.subscribe(() => {
+						localStorage.removeItem(environment.accessToken);
+						this.notificationService.success('Logged off.');
+						this.ngOnInit();
+						this.router.navigate(['home']);
+					}, err => console.log(err));
+			}
+		});
+		this.subscriptions.push(this.dialogRefSubscription1);
+
+	}
+
+	onCreateAccountClick(): void {
+		const dialogConfig = new MatDialogConfig();
+		dialogConfig.autoFocus = false;
+		dialogConfig.height = "85%";
+		dialogConfig.width = "35%";
+		this.matDialog.open(UserRegistrationComponent, dialogConfig);
+	}
 
 	private loadPuzzleTypesFromApi() {
 		this.lookupService.getPuzzleTypes()
@@ -63,5 +110,11 @@ export class AppComponent implements AfterViewInit, OnInit {
 	ngAfterViewInit(): void {
 	}
 
-
+	ngOnDestroy(): void{
+		this.subscriptions.forEach(s => {
+			if (s) {
+				s.unsubscribe();
+			}
+		});
+	}
 }
